de5924aeafc1284f5844c3db8196f480a92a6b79
shouravmisro <mshourav05@gmail.com>
2025-08-31 21:09:40 +0600

    fix app.py

--- app.py
+++ app.py
@@ -50,6 +50,6 @@ def weather():
 
 if __name__ == "__main__":
     with app.app_context():
-        db.create_all()  # Create DB tables if not exist
+        db.create_all()
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
